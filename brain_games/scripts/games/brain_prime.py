efa820f2f0a44a5a04fd617032d330120b2aac3c
fooxeed <fooxeed@mail.ru>
2023-10-22 00:34:12 +1000

    Refactoring 01

--- brain_games/scripts/games/brain_prime.py
+++ brain_games/scripts/games/brain_prime.py
@@ -11,38 +11,29 @@ def my_prime():
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     r_num = randint(1, 100)
     number_prime = check_prime(r_num)
-    print('Question: ' + str(r_num))
+    print('Question: ' + str(r_num) + '(' + str(number_prime) + ')')
     f_num = prompt.string('Your answer: ')
     if f_num == 'yes' and number_prime is True or f_num == 'no' and number_prime is False:
         print('Correct!')
         r_num2 = randint(1, 100)
         number_prime2 = check_prime(r_num2)
-        print('Question: ' + str(r_num2))
+        print('Question: ' + str(r_num2) + '(' + str(number_prime2) + ')')
         f_num2 = prompt.string('Your answer: ')
         if f_num2 == 'yes' and number_prime2 is True or f_num2 == 'no' and number_prime2 is False:
             print('Correct!')
             r_num3 = randint(1, 100)
             number_prime3 = check_prime(r_num3)
-            print('Question: ' + str(r_num3))
+            print('Question: ' + str(r_num3) + '(' + str(number_prime3) + ')')
             f_num3 = prompt.string('Your answer: ')
             if f_num3 == 'yes' and number_prime3 is True or f_num3 == 'no' and number_prime3 is False:
                 print('Correct!')
                 print('Congratulations, ' + name + '!')
-            elif f_num3 == 'yes' and number_prime3 is False or f_num3 == 'no' and number_prime3 is True:
-                print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-                print("Let's try again, " + name + "!")
             else:
                 print("'" + str(f_num3) + "' is wrong answer ;(. Correct answer was 'no'.")
                 print("Let's try again, " + name + "!")
-        elif f_num2 == 'yes' and number_prime2 is False or f_num2 == 'no' and number_prime2 is True:
-            print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-            print("Let's try again, " + name + "!")
         else:
             print("'" + str(f_num2) + "' is wrong answer ;(. Correct answer was 'no'.")
             print("Let's try again, " + name + "!")
-    elif f_num == 'yes' and number_prime is False or f_num == 'no' and number_prime is True:
-        print("'yes' is wrong answer ;(. Correct answer was 'no'.")
-        print("Let's try again, " + name + "!")
     else:
         print("'" + str(f_num) + "' is wrong answer ;(. Correct answer was 'no'.")
         print("Let's try again, " + name + "!")
